826032723ba01df757bbfcff842a0e1e1cbca724
Mostafa Magdy <mostafa.magdy621@gmail.com>
2022-03-29 01:48:28 +0200

    change to patch

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,7 +5,7 @@ const {isAuthenticated,} = require('../middlewares/admin')
 
 router.post("/addProduct",isAuthenticated,addProduct);
 router.post("/deleteProduct",isAuthenticated,deleteProduct);
-router.post("/updateProduct",isAuthenticated,updateProduct);
+router.patch("/updateProduct",isAuthenticated,updateProduct);
 router.get("/getAllProducts",get_all_products);
 router.get("/getProductById/:id",getProductById);
 
